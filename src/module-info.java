a88ddf0a3df435ddfa64e52cb0a27bd0e526e1a1
gedeaoaraujo <gedeao.araujo@outlook.com.br>
2020-04-02 19:06:25 -0300

    First commit

---
+++ src/module-info.java
@@ -0,0 +1,6 @@
+module NovoJavaFXTeste {
+    requires javafx.fxml;
+    requires javafx.controls;
+
+    opens sample;
+}
---
+++ src/sample/Controller.java
@@ -0,0 +1,9 @@
+package sample;
+
+import javafx.scene.control.Button;
+
+public class Controller {
+
+    Button button;
+
+}
